9a647bc40e138defc48eed7ac4b15ff6b62b3d6b
iceaccold <83141913+iceaccold@users.noreply.github.com>
2021-06-11 13:30:41 -0700

    Update proj03.asm

--- proj03.asm
+++ proj03.asm
@@ -1,9 +1,7 @@
 TITLE Programming Assignment 3 - Fibonacci Sequence (Fibonacci_numbers.asm)
 
-; Author: 
-; CSC 221 / Program 03
+; Program 03
 ; Description:  Fibonacci sequence using  a loop
-; DUE DATE:
 
 ; Fibonacci is a sequence of number that sums up the previous two numbers in the
 ; sequence. The first two numbers are 1, and once they are added up, they equal the
@@ -18,8 +16,7 @@ INCLUDE Macros.inc
 .data
 
 welcome	BYTE   "----------------------------------------------------			", 13, 10,
-	           " CSC 221 Project 3: Fibonacci sequence using a loop				", 13, 10,
-	           " Author: Adam Chavez											", 13, 10,
+	           " Project 3: Fibonacci sequence using a loop				", 13, 10,
 	           " Calculating the Fibonacci Numbers up to number asked.			", 13, 10,
 	           "----------------------------------------------------			",0
 
@@ -30,41 +27,41 @@ num2 BYTE ?
 
 
 .code
-	main PROC							; start of the MAIN prodecure 
+	main PROC					; start of the MAIN prodecure 
 		mov edx, OFFSET welcome			; move the address of greeting into edx register      
-		call WriteString				; write out the greeting 
-		call CrLf						; write out a newline (carriage-return Form-feed) 
+		call WriteString			; write out the greeting 
+		call CrLf				; write out a newline (carriage-return Form-feed) 
 
 		mov edx, OFFSET prompt			; move to the address of prompt to edx
-		call WriteString				; write out the prompt
-		call ReadDec					; user value is put into eax
-		call CrLf						; write out a newline (carriage-return Form-feed) 
+		call WriteString			; write out the prompt
+		call ReadDec				; user value is put into eax
+		call CrLf				; write out a newline (carriage-return Form-feed) 
 
-		mov ecx, eax					; mov value to loop counter
+		mov ecx, eax				; mov value to loop counter
 
-		dec ecx							; subtract 1 because we are starting with 0 and printing 0
-		mov al, num0					; stores the first number in num0
-		call WriteDec					; display first number 0
-		mWriteSpace						; writes a space infront of the first value
+		dec ecx					; subtract 1 because we are starting with 0 and printing 0
+		mov al, num0				; stores the first number in num0
+		call WriteDec				; display first number 0
+		mWriteSpace				; writes a space infront of the first value
 
-		dec ecx							; subtract 1 because we are directly printing second number
-		mov al, num1					; moves the second number into al
-		call WriteDec					; displays the second number 1
-		mWriteSpace						; writes a space infront of the second value
+		dec ecx					; subtract 1 because we are directly printing second number
+		mov al, num1				; moves the second number into al
+		call WriteDec				; displays the second number 1
+		mWriteSpace				; writes a space infront of the second value
 
-	LOOPY:								; start of loop LOOPY
-		mov al, num0					; move num0 to al
-		add al, num1					; add num1 to al
-		mov num2, al					; move the value in al into num2
+	LOOPY:						; start of loop LOOPY
+		mov al, num0				; move num0 to al
+		add al, num1				; add num1 to al
+		mov num2, al				; move the value in al into num2
 
-		mov bl, num1					; move num1 into bl 
-		mov num0, bl					; move the value in bl into num0
+		mov bl, num1				; move num1 into bl 
+		mov num0, bl				; move the value in bl into num0
 
-		mov num1, al					; move the value of al to num1				
-		call WriteDec					; write the value as a decimal
-		mWriteSpace						; write a space infront of the new value
-	loop LOOPY							; go to top_of_loop if and only if (--ecx) == 0
+		mov num1, al				; move the value of al to num1				
+		call WriteDec				; write the value as a decimal
+		mWriteSpace				; write a space infront of the new value
+	loop LOOPY					; go to top_of_loop if and only if (--ecx) == 0
 	
-	exit								; Exit to the operating system
-	main ENDP							; end of MAIN procedure 
-END main								; program tell MASM to execute start the MAIN procedure 
+	exit						; Exit to the operating system
+	main ENDP					; end of MAIN procedure 
+END main						; program tell MASM to execute start the MAIN procedure 
